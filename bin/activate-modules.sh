e506b6edcbff0190d030a1810266b115fe8f023e
Petr Podrabsky <petrpo@gmail.com>
2014-01-17 19:57:50 +0100

    CHANGE: changed filename

---
+++ bin/activate-modules.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+MODULES="apr apr-util httpd php"
+for i in $MODULES
+do
+    echo "$i : "
+    slax download $i
+    slax activate $i
+done
+
+echo
+echo "Necessary modules activated"
+
+ without output
+/usr/sbin/httpd & > /dev/null
+
+echo
+echo "httpd started in Slax"
+
+
