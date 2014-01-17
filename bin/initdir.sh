6def7579383efb77c5f975fd7653fbae40ced60c
Petr Podrabsky <petrpo@gmail.com>
2014-01-17 20:55:00 +0100

    CHANGE: added copy files to www dir, run firefox

--- bin/initdir.sh
+++ bin/initdir.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 2014-01-17 init script initilize directory "~", downloads repo, add Slax modules, copies files to /var/www 
 
+WEB_DIR="/var/www/htdocs"
 HOME_DIR="`pwd`"
 TMP_DIR="tmp"
 GITHUB="https://github.com/petrpo"
@@ -20,4 +21,9 @@ unzip $MASTER -d $TMP_DIR
 bash $HOME_DIR/$TMP_DIR/$REPO_MASTER/bin/activate-modules.sh
 
 echo "Modules for Jbox are activated"
+echo 
+
+cp $HOME_DIR/$TMP_DIR/$REPO_MASTER/www/ $WEB_DIR
+
+/usr/bin/firexox http://localhost/
 
