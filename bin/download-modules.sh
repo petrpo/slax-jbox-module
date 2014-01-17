474cf394c2647a0688268e2ea7cf2b9916e50df5
Petr Podrabsky <petrpo@gmail.com>
2014-01-17 18:38:31 +0100

    CHANGE: add cycle for and list of modules

--- bin/download-modules.sh
+++ bin/download-modules.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-slax download php httpd
-slax activate php httpd
+MODULES="php httpd"
+for i in $MODULES
+do
+    printf "$i : "
+    slax download $i
+    slax activate $i
+done
+
+#slax download php httpd
+#slax activate php httpd
 
 
