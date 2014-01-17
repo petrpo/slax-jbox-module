a9d4682bc31d315f1ab65b1e59112c2c4053308b
Petr Podrabsky <petrpo@gmail.com>
2014-01-17 20:40:09 +0100

    CHANGE: tunning paths to script

--- bin/initdir.sh
+++ bin/initdir.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # 2014-01-17 init script initilize directory "~", downloads repo, add Slax modules, copies files to /var/www 
 
-TMP_DIR="tmp/"
-GITHUB="https://github.com/petrpo/"
-REPO="slax-jbox-module/"
+HOME_DIR="`pwd`"
+TMP_DIR="tmp"
+GITHUB="https://github.com/petrpo"
+REPO="slax-jbox-module"
 REPO_MASTER="slax-jbox-module-master"
-ARCH="archive/"
+ARCH="archive"
 MASTER="master.zip"
 
-mkdir $TMP_DIR
+mkdir $HOME_DIR/$TMP_DIR
+#echo $HOME_DIR$TMP_DIR
+#exit
 
-wget $GITHUB$REPO$ARCH$MASTER
+wget $GITHUB/$REPO/$ARCH/$MASTER
 
 unzip $MASTER -d $TMP_DIR
 
-bash ~/$REPO_MASTER/bin/activate-modules.sh
+bash $HOME_DIR/$TMP_DIR/$REPO_MASTER/bin/activate-modules.sh
 
 echo "Modules for Jbox are activated"
 
