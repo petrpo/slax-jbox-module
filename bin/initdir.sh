69fafceb7cd96631040cf723cf71cad477f7b77c
Petr Podrabsky <petrpo@gmail.com>
2014-01-17 20:28:28 +0100

    CHANGE : midified file (right paths for unzipping)

--- bin/initdir.sh
+++ bin/initdir.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # 2014-01-17 init script initilize directory "~", downloads repo, add Slax modules, copies files to /var/www 
 
-TMP_DIR="~/tmp"
+TMP_DIR="tmp/"
 GITHUB="https://github.com/petrpo/"
 REPO="slax-jbox-module/"
+REPO_MASTER="slax-jbox-module-master"
 ARCH="archive/"
 MASTER="master.zip"
 
-mkdir TMP_DIR
+mkdir $TMP_DIR
 
 wget $GITHUB$REPO$ARCH$MASTER
 
 unzip $MASTER -d $TMP_DIR
 
-bash ~/$REPO/bin/activate-modules.sh
+bash ~/$REPO_MASTER/bin/activate-modules.sh
 
+echo "Modules for Jbox are activated"
 
