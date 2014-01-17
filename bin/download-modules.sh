c38014b7614567e1280b924d3faa3b628fec86c8
Petr Podrabsky <petrpo@gmail.com>
2014-01-17 19:39:49 +0100

    CHANGE: changing things to download, unzip it and execute on local

--- bin/download-modules.sh
+++ bin/download-modules.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-MODULES="apr apr-util httpd php"
-for i in $MODULES
-do
-    printf "$i : "
-    slax download $i
-    slax activate $i
-done
+TMP_DIR="~/tmp"
+REPO_ARCH="https://github.com/petrpo/slax-jbox-module/archive/"
+MASTER="master.zip"
 
-echo
-echo "Necessary modules activated"
-
+#MODULES="apr apr-util httpd php"
+#for i in $MODULES
+#do
+#    echo "$i : "
+#    slax download $i
+#    slax activate $i
+#done
+#
+#echo
+#echo "Necessary modules activated"
+#
 # without output
-/usr/sbin/httpd & > /dev/null
+#/usr/sbin/httpd & > /dev/null
+#
+#echo
+#echo "httpd started in Slax"
 
-echo
-echo "httpd started in Slax"
+mkdir TMP_DIR
 
+wget $REPO_MASTER
+
+unzip $REPO_ARCH$MASTER -d $TMP_DIR
