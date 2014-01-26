e233ac5d37fdd624e383b14491b1a40a465c4d28
Petr Podrabsky <petrpo@gmail.com>
2014-01-26 09:23:10 +0100

    CHANGE: add dependency icu4c for boost

--- bin/compile-webtoolkit.sh
+++ bin/compile-webtoolkit.sh
@@ -5,6 +5,7 @@ SRC_FILE="wt-3.3.1.tar.gz"
 URL="http://kent.dl.sourceforge.net/project/witty/wt/3.3.1/"
 
 # download dependencies to compile wt
+slax activate icu4c
 slax activate boost
 slax buildscript download template
 
