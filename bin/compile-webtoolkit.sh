6f8721f5f3db128e3f20c198d843291e7d61badb
Petr Podrabsky <petrpo@gmail.com>
2014-01-26 09:17:27 +0100

    CHANGE: correction of path of DWLOADED src of WT

--- bin/compile-webtoolkit.sh
+++ bin/compile-webtoolkit.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # 2014-01-26 first init of 
 
-SRC_FILE="wget http://kent.dl.sourceforge.net/project/witty/wt/3.3.1/wt-3.3.1.tar.gz"
+SRC_FILE="wt-3.3.1.tar.gz"
+URL=http://kent.dl.sourceforge.net/project/witty/wt/3.3.1/"
 
-wget $SRC_FILE
+# download dependencies to compile wt
+slax activate boost
+slax buildscript download template
+
+wget $URL$SRC_FILE
 tar -xvzpf $SRC_FILE
 cd $SRC_FILE
-
-slax buildscript download template
+mkdir build
+cd build
+cmake ..
