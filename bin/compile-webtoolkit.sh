b147335e263b6ff4664bab6bdb173c9378465496
Petr Podrabsky <petrpo@gmail.com>
2014-01-26 10:19:33 +0100

    CHANGE: corrections how to get a name of dir

--- bin/compile-webtoolkit.sh
+++ bin/compile-webtoolkit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 2014-01-26 first init of 
 
-SRC_FILE="wt-3.3.1.tar.gz"
+SRC_FILE="wt-3.3.1"
 URL="http://kent.dl.sourceforge.net/project/witty/wt/3.3.1/"
 
 # download dependencies to compile wt
@@ -9,9 +9,9 @@ slax activate icu4c
 slax activate boost
 slax buildscript download template
 
-wget $URL$SRC_FILE
+wget $URL$SRC_FILE.tar.gz
 tar -xvzpf $SRC_FILE
-cd `echo $SRC_FILE | awk -F.tar.gz '{print $1}'`
+cd $SRC_FILE
 mkdir build
 cd build
 cmake ..
