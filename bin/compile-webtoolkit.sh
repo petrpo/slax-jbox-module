f31b5555a34d6d52c28fcc9d6c95fcd093efe9d8
Petr Podrabsky <petrpo@gmail.com>
2014-01-26 09:20:15 +0100

    CHANGE: added missing "

--- bin/compile-webtoolkit.sh
+++ bin/compile-webtoolkit.sh
@@ -2,7 +2,7 @@
 # 2014-01-26 first init of 
 
 SRC_FILE="wt-3.3.1.tar.gz"
-URL=http://kent.dl.sourceforge.net/project/witty/wt/3.3.1/"
+URL="http://kent.dl.sourceforge.net/project/witty/wt/3.3.1/"
 
 # download dependencies to compile wt
 slax activate boost
