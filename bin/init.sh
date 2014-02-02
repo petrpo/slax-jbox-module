ebf42ed7b3c7f419efa00a834c0e7cd3e66da738
Petr Podrabsky <petrpo@gmail.com>
2014-02-02 10:32:25 +0100

    CHANGE: script overwritten

--- bin/init.sh
+++ bin/init.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 # 2014-01-17 init script initilize directory "~", downloads repo, add Slax modules, copies files to /var/www 
+# 2014-02-02 init script changed to easier repo downloading with git which has to be activated
 
 WEB_DIR="/var/www/htdocs"
 HOME_DIR="`pwd`"
-TMP_DIR="tmp"
+
 GITHUB="https://github.com/petrpo"
-REPO="slax-jbox-module"
-REPO_MASTER="slax-module-master"
-ARCH="archive"
-MASTER="master.zip"
+REPO="slax-module.git"
+REPO_DIR="slax-module"
 ROOT_MYSQL="toor"
 
-mkdir $HOME_DIR/$TMP_DIR
 slax activate git
 git clone https://github.com/petrpo/slax-module.git
 
-exit
-
-#echo $HOME_DIR$TMP_DIR
-#exit
-
-wget $GITHUB/$REPO/$ARCH/$MASTER
-
-unzip $MASTER -d $TMP_DIR
-
-bash $HOME_DIR/$TMP_DIR/$REPO_MASTER/bin/activate-modules.sh
+# activate apache & php
+bash $HOME_DIR/$REPO_DIR/bin/activate-apache-php.sh
 
 echo "Modules for Jbox are activated"
 echo 
@@ -33,8 +23,6 @@ cp $HOME_DIR/$TMP_DIR/$REPO_MASTER/www/* $WEB_DIR
 
 echo "Include /etc/httpd/mod_php.conf" >> /etc/httpd/httpd.conf
 echo
-
-
 
 mysqladmin -u root password $MYSQL_ROOT
 echo "Mysql root password created: $MYSQL_ROOT"
